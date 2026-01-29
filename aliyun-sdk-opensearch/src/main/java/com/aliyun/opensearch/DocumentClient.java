3c9ad92e0242645104814ad97aa090e43c14ae04
lyk195 <57087288+lyk195@users.noreply.github.com>
2026-01-29 15:53:18 +0800

    Update DocumentClient.java
    推送文档支持upsert

--- aliyun-sdk-opensearch/src/main/java/com/aliyun/opensearch/DocumentClient.java
+++ aliyun-sdk-opensearch/src/main/java/com/aliyun/opensearch/DocumentClient.java
@@ -62,6 +62,18 @@ public class DocumentClient implements DocumentService.Iface {
   }
 
   /**
+   * 更新插入文档，如果该主键对应文档已经存在则执行update，否则执行add。
+   *
+   * 标准版不支持upsert
+   *
+   * @param fields 字段名和字段值的map
+   */
+  public void upsert(Map<String, Object> fields) {
+	pushOneDoc(fields, Command.UPSERT);
+  }
+
+
+  /**
    * 删除文档
    *
    * 设置需要删除的属性名称和属性值，用于生成符合文档格式的数据，所有更新结束之后需要调用push(String tableName)方法
@@ -69,11 +81,11 @@ public class DocumentClient implements DocumentService.Iface {
    * @param fields 字段名和字段值的map
    */
   public void remove(Map<String, Object> fields) {
-    pushOneDoc(fields, Command.DELETE);
+	pushOneDoc(fields, Command.DELETE);
   }
 
   private void pushOneDoc(Map<String, Object> fields, Command command) {
-    JSONObject JSONFields = new JSONObject();
+  	JSONObject JSONFields = new JSONObject();
     for (Entry<String, Object> entry : fields.entrySet()) {
       JSONFields.put(entry.getKey(), entry.getValue());
     }
